99bb86ba2960824a0511b517eec20797211e022e
notzak <zakabarir@gmail.com>
2023-07-13 21:41:23 -0700

    please dont crash again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,12 +16,12 @@ int main(void)
 
 	printf("%d ", n);
 
-		if (n > 0)
-			printf("is positive\n");
-		else if (n == 0)
-			printf("is zero\n");
-		else
-			printf("is negative\n");
+	if (n > 0)
+	printf("is positive\n");
+	else if (n == 0)
+		printf("is zero\n");
+	else
+		printf("is negative\n");
 
 	return (0);
 }

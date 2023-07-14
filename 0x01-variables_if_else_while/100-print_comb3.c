4fce5d9aae88eb80f67c05a7d796d0274ddcac30
notzak <zakabarir@gmail.com>
2023-07-14 10:50:18 -0700

    hehe

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,7 +19,7 @@ int main(void)
             if (i != 8 || j != 9)
             {
                 putchar(',');
-                putchar(' ');
+                putchar('\t');
             }
         }
     }
@@ -27,4 +27,4 @@ int main(void)
     putchar('\n');
 
     return 0;
-}
+}}

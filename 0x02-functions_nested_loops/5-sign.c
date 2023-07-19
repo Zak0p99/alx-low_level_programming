7961ef388d44b81378b958d69a3504fb5ce19cea
notzak <zakabarir@gmail.com>
2023-07-18 18:35:29 -0700

    aaa

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-#include <limits.h>
 #include "main.h"
 
 /**
@@ -10,38 +9,17 @@
  */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        } 
-        else if (n == 0)
-        {
-                _putchar('0');
-                return (0);
-        } 
-        else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
-        }
-        
-        return (0); /* Add a default return statement here */
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
 }
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-        int r;
-
-        r = print_sign(98);
-        if (r < 0)
-                r *= -1;
-        _putchar(r + '0');
-        _putchar('\n');
-        return (0);
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
